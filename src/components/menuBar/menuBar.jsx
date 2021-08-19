ab434c5b41ca2a2054efd2de5f6a7134a9074a8c
AnaLinsDev <analinsdev@gmail.com>
2021-08-19 20:35:04 -0300

    adicionando menu, about me e skills

---
+++ src/components/menuBar/menuBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './menuBar.scss'
+
+const menuBar = () => {
+
+    return (
+    <div className='menu'>
+        <a href="#aboutme">About me</a>
+        <a href="#myskills">Skills</a>
+        <a href="#myprojects">Projects</a>
+    </div>
+    )
+    }
+
+export default menuBar 
---
+++ src/components/myProjects/myProjects.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './myProjects.scss'
+
+const MyProjects = () => {
+
+    return (
+    <div className='projects' id='myprojects'>
+        <hr className='line'></hr>
+        <p className='title'>Projetos: </p>
+        <div className='conteiner'>
+            <div className='projetoPreview'><h1>PROJETO AQUI</h1></div>
+            <div className='projetoPreview'><h1>PROJETO AQUI</h1></div>
+            <div className='projetoPreview'><h1>PROJETO AQUI</h1></div>
+            <div className='projetoPreview'><h1>PROJETO AQUI</h1></div>
+            <div className='projetoPreview'><h1>PROJETO AQUI</h1></div>
+            <div className='projetoPreview'><h1>PROJETO AQUI</h1></div>
+            <div className='projetoPreview'><h1>PROJETO AQUI</h1></div>
+        </div>
+    </div>
+    )
+    }
+
+export default MyProjects 
